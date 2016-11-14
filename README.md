bd7e1a1237c6dc3d3550217242459e7dd63124f4
Travis Roundy <travis.roundy@colorado.edu>
2016-11-14 12:06:31 -0700

    added ability to split data into training/testing VW files in parsey.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MLFinalProject
+
+USAGE: python parser.py flag trainPercentage
+
+	flag = VW for creating VW datafile
+	       TT for creating Testing/Training randomly with the VW file
+	trainPercentage = number from 0-1, splits VW file accordingly.
--- parser.py
+++ parser.py
@@ -1,24 +1,65 @@
 import csv
+import sys
+from random import *
 
-outfile = open('300k.vw', 'w')
-count_file = open('pokemon_summary.csv', 'w')
+def makeVW():
+	outfile = open('300k.vw', 'w')
+	count_file = open('pokemon_summary.csv', 'w')
 
-pokemon_counts = [0] * 151
-with open("300k.csv") as infile:
-    first=True
-    for row in csv.reader(infile):
-        if(first == True):
-            first=False
-            headers = row
-        else:
-            pokemon_counts[int(row[0])]+=1
-            params_string = ""
-            for i in range(1,len(row)):
-                params_string+=("{}:{} ".format(headers[i], row[i]))
+	pokemon_counts = [0] * 151
+	with open("300k.csv") as infile:
+		first=True
+		for row in csv.reader(infile):
+			if(first == True):
+				first=False
+				headers = row
+			else:
+				pokemon_counts[int(row[0])]+=1
+				params_string = ""
+				for i in range(1,len(row)):
+					if i != 3:
+						params_string+=("{}:{} ".format(headers[i], row[i]))
 
-            write_string = "{} | {} \n".format(row[0], params_string)
-            outfile.write(write_string)
-count_file.write(str(pokemon_counts[1:-2]))
-outfile.close()
+				write_string = "{} | {} \n".format(row[0], params_string)
+				outfile.write(write_string)
+	count_file.write(str(pokemon_counts[1:-2]))
+	outfile.close()
+	return
+	
+def testTrain(percentage):
+	testfile = open('test.vw', 'w')
+	trainfile = open('train.vw', 'w')
+	f = open('300k.vw')
+
+	doubleItems = []
+
+	for line in f.readlines():
+		doubleItems.append(line)
+	f.close()
+	
+	perTest = percentage
+	perTrain = 1 - percentage
+	size = len(doubleItems)
+	trainSize = int(perTrain * size)
+	rand = sample(range(0,size), size)
+	for number in range(0,trainSize):
+		trainfile.write(doubleItems[rand[number]])
+	for number2 in range(trainSize, size):
+		testfile.write(doubleItems[rand[number2]])
+		
+	return
+
+def main():
+	option = sys.argv[1]
+	
+	if option == "VW":
+		makeVW()
+	if option == "TT":
+		percentage = sys.argv[2]
+		if percentage > 1:
+			percentage = .33
+		testTrain(percentage)
 
 
+if __name__ == "__main__":
+	main()
