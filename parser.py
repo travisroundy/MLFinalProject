246d72b7515a01af607efe725b809c83b2587ce2
Justin <justinwayneolson@gmail.com>
2016-11-15 10:34:49 -0700

    added lookup table stuff

--- parser.py
+++ parser.py
@@ -1,8 +1,13 @@
 import csv
 import sys
 from random import *
+import json
+
+with open('lookup.json') as input_file:
+    lookup = json.load(input_file)
 
 def makeVW():
+        ignore = ["_id", "appearedLocalTime"]
 	outfile = open('300k.vw', 'w')
 	count_file = open('pokemon_summary.csv', 'w')
 
@@ -17,15 +22,22 @@ def makeVW():
 				pokemon_counts[int(row[0])]+=1
 				params_string = ""
 				for i in range(1,len(row)):
-					if i != 3:
-						params_string+=("{}:{} ".format(headers[i], row[i]))
+                                    if(headers[i] in ignore):
+                                        continue
+                                    if(headers[i] in lookup.keys()):
+                                        row[i]=lookup[headers[i]][row[i]]
+                                    params_string+=("{}:{} ".format(headers[i], row[i]))
 
 				write_string = "{} | {} \n".format(row[0], params_string)
+                                write_string = write_string.replace('TRUE', '1')
+                                write_string = write_string.replace('true', '1')
+                                write_string = write_string.replace('FALSE', '0')
+                                write_string = write_string.replace('false', '0')
 				outfile.write(write_string)
 	count_file.write(str(pokemon_counts[1:-2]))
 	outfile.close()
 	return
-	
+
 def testTrain(percentage):
 	testfile = open('test.vw', 'w')
 	trainfile = open('train.vw', 'w')
@@ -36,7 +48,7 @@ def testTrain(percentage):
 	for line in f.readlines():
 		doubleItems.append(line)
 	f.close()
-	
+
 	perTest = percentage
 	perTrain = 1 - percentage
 	size = len(doubleItems)
@@ -46,12 +58,12 @@ def testTrain(percentage):
 		trainfile.write(doubleItems[rand[number]])
 	for number2 in range(trainSize, size):
 		testfile.write(doubleItems[rand[number2]])
-		
+
 	return
 
 def main():
 	option = sys.argv[1]
-	
+
 	if option == "VW":
 		makeVW()
 	if option == "TT":
